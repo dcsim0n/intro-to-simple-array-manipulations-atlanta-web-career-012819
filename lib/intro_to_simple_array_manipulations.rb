868561f3d58ea42ef7ecaf06cc3d64b236cfd60f
Dana Simmons <dsanalog@gmail.com>
2019-01-22 18:19:24 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end 
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(first_array, second_array)
+  return first_array.concat(second_array)
+end
+
+def using_insert(array,e)
+  array.insert(e,3)
+end
